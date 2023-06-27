34cad73c95cb2a29707129600ac08e10bb7324c1
Luiz Rodriguez Fantini <fantini@celepar.pr.gov.br>
2023-06-27 11:04:36 -0300

    Revisao AMQP

--- src/main/java/br/com/fantini/amqp/business/MessageBusiness.java
+++ src/main/java/br/com/fantini/amqp/business/MessageBusiness.java
@@ -3,14 +3,21 @@ package br.com.fantini.amqp.business;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
 
 import br.com.fantini.amqp.commons.Message;
+import io.smallrye.reactive.messaging.annotations.Blocking;
 import jakarta.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
 public class MessageBusiness {
-    
+    /*
+     * @Blocking: 
+     *  - identifica que metodo ira realizar operacoes bloqueantes e permitira sua execucao em um worker separado
+     * @ActivateRequestContext:
+     *  - deve ser utilizado caso as operacoes envolvidades necessitem do contexto. Exemplo: acesso ao DB
+     */
     @Incoming("messages-in")
+    @Blocking
     public void processMessage(Message message) {
-        System.out.println(message.getPayload());
+        System.out.println("Mensagem: " + message.getPayload());
     }
 
 }
